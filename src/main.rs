4f9c088944c85ea5ea9a146eae9ccf4f3d4b9b1a
Max <mr.awesome10000@gmail.com>
2024-08-03 18:52:17 -0700

    loading puzzles

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,40 @@
 use std::collections::HashSet;
 use std::fmt;
 use std::fs;
+use core::str::FromStr;
+use std::num::IntErrorKind;
+use std::path::Path;
 
-use serde::{Serialize, Deserialize};
+#[derive(Debug)]
+struct CustomParseError {
+    kind: IntErrorKind,
+    character: char,
+}
+
+impl fmt::Display for CustomParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Failed to parse '{}': {:?}", self.character, self.kind)
+    }
+}
+
+impl std::error::Error for CustomParseError {}
+
+impl From<(IntErrorKind, char)> for CustomParseError {
+    fn from((kind, character): (IntErrorKind, char)) -> Self {
+        CustomParseError {kind, character}
+    }
+}
+
 
 
 struct Board {
-    arr: [[HashSet<u8>; 10]; 10]
+    size: u8,
+    arr: [[HashSet<u8>; 9]; 9]
 }
 
 impl Default for Board {
     fn default() -> Board {
-        let mut array: [[HashSet<u8>; 10]; 10] = Default::default();
+        let mut array: [[HashSet<u8>; 9]; 9] = Default::default();
 
         for row in array.iter_mut() {
             for i in row.iter_mut() {
@@ -19,7 +42,7 @@ impl Default for Board {
             }
         }
 
-        return Board {arr: array};
+        return Board {size: 9, arr: array};
     }
 }
 
@@ -39,20 +62,60 @@ impl fmt::Display for Board {
     }
 }
 
+impl FromStr for Board {
+    type Err = CustomParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut b: Board = Default::default();
+        let mut line = 0;
+        let mut column = 0;
+        for c in s.chars() {
+            if c == '0' {
+                column += 1;
+                continue;
+            } else if c == '-' {
+                line += 1;
+                column = 0;
+                continue;
+            }
+
+            let err: CustomParseError = (IntErrorKind::InvalidDigit, c).into();
+
+            let digit_u32 = c.to_digit(10).ok_or(err)?;
+            let digit: u8 = digit_u32 as u8;
+
+            let set = HashSet::from([digit]);
+
+            b.arr[line][column] = set;
+
+            column += 1;
+        }
+        return Ok(b);
+    }
+}
 
 
-
-fn main() {
-    let mut board: Board = Default::default();
-
-    let data = fs::read_to_string("./src/boards.json")
+fn read_json<P: AsRef<Path>>(path: P) -> serde_json::Value {
+    let data = fs::read_to_string(path)
         .expect("Unable to read file");
 
     let json: serde_json::Value = serde_json::from_str(data.as_str())
         .expect("JSON was not well-formatted");
+    return json;
+}
 
-    println!("{}", board);
-    println!("{}", json["easy"]);
+fn main() {
+
+    let json = read_json("./src/boards.json");
+
+    let easy = json["easy"].to_string();
+
+    let mut chars = easy.chars();
+    chars.next();
+    chars.next_back();
+    let stripped = chars.as_str();
+
+    println!("{}", Board::from_str(stripped).expect("err"));
     
 
 
